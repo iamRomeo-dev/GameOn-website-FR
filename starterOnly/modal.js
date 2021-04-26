ae0c51a4c9f2ef67d24b8feb089349e848277164
iamRomeo-dev <romeo.brilland@live.fr>
2021-04-26 16:28:18 +0200

    :zap: add checkbox and date validation. Improve the design

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const msgSuccess = document.getElementById("msgSuccessId");
+const buttonClose = document.getElementById("buttonClose");
+const crossClose = document.getElementById("crossClose");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -21,51 +23,54 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//=========================================
+
 // close modal event
 document.querySelector(".close").addEventListener("click", () => {
   modalbg.style.display = "none";
 });
 
 // close modal event
-msgSuccess.addEventListener("click", () => {
+buttonClose.addEventListener("click", () => {
+  msgSuccess.style.display = "none";
+});
+crossClose.addEventListener("click", () => {
   msgSuccess.style.display = "none";
 });
 
-//=========================================
-
-let form = document.getElementById("form");
-let firstNameInput = document.getElementById("first");
-let lastNameInput = document.getElementById("last");
-
+//FUNCTION VALIDATION OF EMAIL WITH REGEX (USE INSIDE THE VALIDATION FUNCTION)
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-form.addEventListener("submit", validation);
-
+//FUNCTION OF INPUTS VALIDATION
 function validation(e) {
   e.preventDefault();
   let firstNameInput = document.getElementById("first");
-  let espacementInput = document.querySelectorAll(".espacementInput");
+  let lastNameInput = document.getElementById("last");
   let email = document.getElementById("email").value;
+  let date = document.getElementById("birthdate").value;
   let quantity = document.getElementById("quantity").value;
-  console.log(quantity.length)
-  //FIRSTNAME
-  if (firstNameInput.value.length < 2) {
+  let checkbox1 = document.getElementById("checkbox1");
+  let espacementInput = document.querySelectorAll(".espacementInput");
+
+  firstNameError.style.display = "none";
+  //VALIDATION FIRSTNAME
+  if (firstNameInput.value.length < 2 || isNaN(firstNameInput.value) === false) {
     let firstNameError = document.getElementById("firstNameError");
     firstNameError.style.display = "block";
 
     espacementInput.forEach((element) => (element.style.display = "none"));
-  } //LASTNAME
-  else if (lastNameInput.value.length < 2) {
+  } //VALIDATION LASTNAME
+  else if (lastNameInput.value.length < 2 || isNaN(lastNameInput.value) === false) {
     firstNameError.style.display = "none";
 
     let lastNameError = document.getElementById("lastNameError");
     lastNameError.style.display = "block";
 
     espacementInput.forEach((element) => (element.style.display = "none"));
-  } //EMAIL
+  } //VALIDATION EMAIL
   else if (!validateEmail(email)) {
     lastNameError.style.display = "none";
 
@@ -73,12 +78,28 @@ function validation(e) {
     emailError.style.display = "block";
 
     espacementInput.forEach((element) => (element.style.display = "none"));
-  } //NUMBERS
-  else if (isNaN(quantity) === true || quantity.length == 0) {
+  } //VALIDATION BIRTHDATE
+  else if (date === "") {
     emailError.style.display = "none";
+
+    let birthdateError = document.getElementById("birthdateError");
+    birthdateError.style.display = "block";
+
+    espacementInput.forEach((element) => (element.style.display = "none"));
+  } //VALIDATION NUMBERS
+  else if (isNaN(quantity) === true || quantity.length == 0) {
+    birthdateError.style.display = "none";
 
     let quantityError = document.getElementById("quantityError");
     quantityError.style.display = "block";
+  } //VALIDATION UTILISATION CONDITIONS
+  else if (!checkbox1.checked) {
+    quantityError.style.display = "none";
+
+    let checkbox1Error = document.getElementById("checkbox1Error");
+    checkbox1Error.style.display = "block";
+
+    espacementInput.forEach((element) => (element.style.display = "none"));
   } //TRUE
   else {
     msgSuccess.style.display = "block";
@@ -87,24 +108,11 @@ function validation(e) {
     lastNameError.style.display = "none";
     emailError.style.display = "none";
     quantityError.style.display = "none";
+    birthdateError.style.display = "none";
+    checkbox1Error.style.display = "none";
   }
 }
 
-// function validation(e) {
-//   e.preventDefault();
-//   if (lastNameInput.value.length < 2 || firstNameInput.value.length < 2) {
-//     let firstNameError = document.getElementById("firstNameError");
-//     firstNameError.style.display = "block";
-
-//     let lastNameError = document.getElementById("lastNameError");
-//     lastNameError.style.display = "block";
-
-//     let espacementInput = document.querySelectorAll(".espacementInput");
-//     espacementInput.forEach((element) => (element.style.display = "none"));
-//   } else {
-//     msgSuccess.style.display = "block";
-
-//     firstNameError.style.display = "none";
-//     lastNameError.style.display = "none";
-//   }
-// }
+//SUBMIT FORM
+let form = document.getElementById("form");
+form.addEventListener("submit", validation);
